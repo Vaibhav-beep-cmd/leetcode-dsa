1ba0835ce51d19720400718d41b07649bca6f5ad
Vaibhav-beep-cmd <vaibhav75735@gmail.com>
2026-07-17 17:47:01 +0530

    Time: 6 ms (21.64%), Space: 46.1 MB (34.17%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0||(x%10==0 && x!=0)){
+            return false;
+        }
+         
+         int reversehalf = 0;
+         while (x > reversehalf){
+            reversehalf = reversehalf * 10 + x % 10;
+            x/=10;
+         }
+         return x == reversehalf || x ==  reversehalf / 10 ;
+
+    }
+}
